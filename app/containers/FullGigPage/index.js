c51fb095a1c73b2aa37c221ee434b31294ae8994
Mark Foster <markfoster314@yahoo.com>
2020-05-05 03:35:54 -0400

    Updated Full Page

--- app/containers/FullGigPage/index.js
+++ app/containers/FullGigPage/index.js
@@ -6,12 +6,24 @@
 
 import React, { memo } from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import { connect } from 'react-redux';
 import { Helmet } from 'react-helmet';
 import { compose } from 'redux';
 import { createStructuredSelector } from 'reselect';
 
 import { makeSelectSelectedGig } from 'containers/App/selectors';
+
+const Inner = styled.div`
+  margin: auto;
+  background: white;
+  border: 1px solid #070600;
+  border-radius: 20px;
+  margin: 20px 40px;
+  @media (max-width: 768px) {
+    margin: 5px 10px;
+  }
+`;
 
 export function FullGigPage({ id }) {
   return (
@@ -20,7 +32,9 @@ export function FullGigPage({ id }) {
         <title>FullGigPage</title>
         <meta name="description" content="Description of FullGigPage" />
       </Helmet>
-      <p>{id}</p>
+      <Inner>
+        <p>{id}</p>
+      </Inner>
     </div>
   );
 }
